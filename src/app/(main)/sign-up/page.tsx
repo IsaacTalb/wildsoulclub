1b7e0e11c33a19502646011034bf70a545406183
Isaac Talb <thaungahlaybaw@gmail.com>
2026-07-14 18:12:29 +0630

    fixed: import supabaseClient

--- src/app/(main)/sign-up/page.tsx
+++ src/app/(main)/sign-up/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { supabase } from "@/src/lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
